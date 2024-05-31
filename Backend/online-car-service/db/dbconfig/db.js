a58bb029234e5a3eb843f4749fbe10c22221d7dc
MaheshNidugala97 <133670096+MaheshNidugala97@users.noreply.github.com>
2024-06-01 01:57:04 +0200

    database connection created for mongo and neo

--- Backend/online-car-service/db/dbconfig/db.js
+++ Backend/online-car-service/db/dbconfig/db.js
@@ -1,17 +1,47 @@
 const mongoose = require('mongoose');
+const neo4j = require('neo4j-driver');
 const { logger } = require('../../util/logging');
+let neo4jDriver;
 
-const connectDB = async () => {
+const connectMongoDB = async () => {
     try {
-        await mongoose.connect('mongodb://localhost:27017/' + process.env.MONGODB_DATABASE, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        await mongoose.connect(
+            'mongodb+srv://' +
+                process.env.MONGODB_USERNAME +
+                ':' +
+                process.env.MONGODB_PASSWORD +
+                '@' +
+                process.env.MONGODB_CLUSTER_URI,
+            {
+                dbName: process.env.MONGODB_DATABASE,
+            }
+        );
         logger.info('Connected to MongoDB');
     } catch (error) {
-        logger.error('Error connecting to MongoDB:', error.message);
+        logger.error('Error connecting to MongoDB:', error);
         process.exit(1);
     }
 };
 
-module.exports = { connectDB };
+const connectNeo4j = async () => {
+    try {
+        neo4jDriver = neo4j.driver(
+            process.env.NEO4J_URI,
+            neo4j.auth.basic(process.env.NEO4J_USERNAME, process.env.NEO4J_PASSWORD)
+        );
+        const session = neo4jDriver.session();
+        logger.info('Connected to Neo4j');
+        return session;
+    } catch (error) {
+        logger.error('Error connecting to Neo4j:', error);
+        process.exit(1);
+    }
+};
+
+const closeNeo4j = async () => {
+    if (neo4jDriver) {
+        await neo4jDriver.close();
+    }
+};
+
+module.exports = { connectMongoDB, connectNeo4j, closeNeo4j };
--- Backend/online-car-service/server.js
+++ Backend/online-car-service/server.js
@@ -1,9 +1,10 @@
 const app = require('./app');
 const { logger } = require('./util/logging');
-// const dbService = require('./db/dbconfig/db');
+const dbService = require('./db/dbconfig/db');
 
 app.listen(process.env.PORT, () => {
     logger.info(`Server running in ${process.env.NODE_ENV} mode on port ${process.env.PORT}`);
 });
 
-// dbService.connectDB();
+dbService.connectMongoDB();
+dbService.connectNeo4j();
