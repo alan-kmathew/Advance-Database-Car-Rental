3cf82e0a0202a70e93e3909a77f594a9f239888c
MaheshNidugala97 <133670096+MaheshNidugala97@users.noreply.github.com>
2024-06-01 16:22:47 +0200

    nearest service station finder api implemented

--- Backend/routes/car.router.js
+++ Backend/routes/car.router.js
@@ -1,7 +1,7 @@
 const { Router } = require('express');
 const { validationResult } = require('express-validator');
 const { logger } = require('../util/logging');
-const { executeShortestPathQuery } = require('../services/car.services');
+const { executeShortestPathQuery, executeNearestServiceStationQuery} = require('../services/car.services');
 const dbService = require('../db/dbconfig/db');
 
 const router = new Router();
@@ -39,5 +39,40 @@ router.get('/get/shortestpath', async (req, res) => {
         });
     }
 });
+
+router.get('/get/nearest/servicestation', async (req, res) => {
+    logger.info(`Entering ${req.baseUrl}${req.path}`);
+    try {
+        const validationErrors = validationResult(req);
+
+        if (!validationErrors.isEmpty()) {
+            const erroMessage = validationErrors.array();
+            return res.status(400).json({
+                timestamp: new Date(),
+                status: 400,
+                error: 'Bad Request',
+                message: erroMessage,
+                path: `${req.baseUrl}${req.path}`,
+            });
+        }
+        const neo4jSession = await dbService.connectNeo4j();
+        const nearestServiceStationResult = await executeNearestServiceStationQuery(
+            neo4jSession,
+            req.query.cityName
+        );
+        return res.status(200).json(nearestServiceStationResult);
+    } catch (err) {
+        logger.error(err);
+        return res.status(500).json({
+            timestamp: new Date(),
+            status: 500,
+            error: 'Bad Request',
+            message: err.message,
+            path: `${req.baseUrl}${req.path}`,
+        });
+    }
+});
+
+
 
 module.exports = router;
