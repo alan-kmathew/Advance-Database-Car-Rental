b5ecdceaa01d306576937ea68f037ee90077f490
Alan K Mathew <sill_mileage_04@icloud.com>
2024-06-05 22:01:17 +0200

    fixed cors issue

--- Backend/app.js
+++ Backend/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors'); 
 const https = require('https');
 const path = require('path');
 const fs = require('fs');
@@ -7,6 +8,7 @@ const carRoutes = require('./routes/car.router');
 
 const app = express();
 
+app.use(cors()); 
 app.use(express.json());
 
 app.use('/', routes);
