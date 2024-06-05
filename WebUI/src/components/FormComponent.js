12506f767190b758c6efebeac6627c42e455e7e5
Alan K Mathew <sill_mileage_04@icloud.com>
2024-06-05 22:07:52 +0200

    Updated Frontend to use data from api

--- WebUI/src/components/FormComponent.js
+++ WebUI/src/components/FormComponent.js
@@ -1,77 +1,75 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
-const FormComponent = ({ locations, onLocationChange, enableSharing }) => {
-  const [fromDate, setFromDate] = useState('');
-  const [toDate, setToDate] = useState('');
-  const [selectedLocation, setSelectedLocation] = useState('');
-  const [destination, setDestination] = useState('');
+const FormComponent = ({ locations, onLocationChange, enableSharing, selectedLocation, fromDate, setFromDate, toDate, setToDate, destination, setDestination, onSearch }) => {
+  const [selectedLocationName, setSelectedLocationName] = useState('');
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const location = locations.find(loc => loc.name === selectedLocation);
-    // You can still handle form submit if needed
-  };
+  useEffect(() => {
+    if (selectedLocation) {
+      setSelectedLocationName(selectedLocation.name);
+    }
+  }, [selectedLocation]);
 
   const handleLocationChange = (e) => {
     const locationName = e.target.value;
-    setSelectedLocation(locationName);
+    setSelectedLocationName(locationName);
     const location = locations.find(loc => loc.name === locationName);
     onLocationChange(location);
   };
 
   return (
-    <form onSubmit={handleSubmit} className="form-container">
- 
-    {!enableSharing && ( 
-      <div>
-        <label>From Date:</label>
-        <input
-          type="date"
-          value={fromDate}
-          onChange={(e) => setFromDate(e.target.value)}
-          required
-        />
-      </div>
-    )}
-    {!enableSharing && ( 
-      <div>
-        <label>To Date:</label>
-        <input
-          type="date"
-          value={toDate}
-          onChange={(e) => setToDate(e.target.value)}
-          required
-        />
-      </div>
-    )}
-      <div>
-        <label>Location:</label>
-        <select
-          value={selectedLocation}
-          onChange={handleLocationChange}
-          required
-        >
-          <option value="">Select a location</option>
-          {locations.map((location, index) => (
-            <option key={index} value={location.name}>
-              {location.name}
-            </option>
-          ))}
-        </select>
-      </div>
-      {enableSharing && (
+    <div>
+      <form onSubmit={(e) => { e.preventDefault(); onSearch(); }} className="form-container">
+        {!enableSharing && ( 
+          <div>
+            <label>From Date:</label>
+            <input
+              type="date"
+              value={fromDate}
+              onChange={(e) => setFromDate(e.target.value)}
+              required
+            />
+          </div>
+        )}
+        {!enableSharing && ( 
+          <div>
+            <label>To Date:</label>
+            <input
+              type="date"
+              value={toDate}
+              onChange={(e) => setToDate(e.target.value)}
+              required
+            />
+          </div>
+        )}
         <div>
-          <label>Destination:</label>
-          <input
-            type="text"
-            value={destination}
-            onChange={(e) => setDestination(e.target.value)}
-            required={enableSharing}
-          />
+          <label>Location:</label>
+          <select
+            value={selectedLocationName}
+            onChange={handleLocationChange}
+            required
+          >
+            <option value="">Select a location</option>
+            {locations.map((location, index) => (
+              <option key={index} value={location.name}>
+                {location.name}
+              </option>
+            ))}
+          </select>
         </div>
-      )}
-      <button type="submit">Search</button>
-    </form>
+        {enableSharing && (
+          <div>
+            <label>Destination:</label>
+            <input
+              type="text"
+              value={destination}
+              onChange={(e) => setDestination(e.target.value)}
+              required={enableSharing}
+            />
+          </div>
+        )}
+        <button type="submit">Search</button>
+      </form>
+    </div>
   );
 };
 
