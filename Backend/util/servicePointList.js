68e8c12d413a0a3025785e930498530313095dd5
MaheshNidugala97 <133670096+MaheshNidugala97@users.noreply.github.com>
2024-06-07 23:07:48 +0200

    Naming convention change

--- Backend/util/servicePointList.js
+++ Backend/util/servicePointList.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const dbService = require('../db/dbconfig/db');
 const { logger } = require('../util/logging');
-const { idToLocation } = require('../util/idToLocation');
+const { idToLocation } = require('./IdToLocation');
 
 const servicePointList = async () => {
     const redisClient = await dbService.connectRedis();
@@ -16,13 +16,18 @@ const servicePointList = async () => {
             return JSON.parse(cachedData);
         }
         const db = mongoose.connection;
-        const servicePoints = await db.collection('servicePoints').find({}, { projection: { _id: 1, name: 1, image: 1 } }).toArray();
+        const servicePoints = await db
+            .collection('servicePoints')
+            .find({}, { projection: { _id: 1, name: 1, image: 1 } })
+            .toArray();
         logger.info('Fetching service points from MongoDB');
 
         // Fetch coordinates for each service point and count the number of cars
         for (let i = 0; i < servicePoints.length; i++) {
             const coordinates = await idToLocation(servicePoints[i]._id);
-            const servicePoint = await db.collection('servicePoints').findOne({ _id: servicePoints[i]._id });
+            const servicePoint = await db
+                .collection('servicePoints')
+                .findOne({ _id: servicePoints[i]._id });
             const carsCount = servicePoint.cars.length;
             servicePoints[i].coordinates = coordinates;
             servicePoints[i].totalCars = carsCount;
